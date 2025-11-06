d591df5cf66ba83ed5404ef85da4877557f137bd
TheTrueFishCake <fishcake2000yt@gmail.com>
2025-11-05 19:42:03 -0500

    Removed some stuff

--- webpark/Assets/Scripts/PlayerController.cs
+++ webpark/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("References")]]
     [SerializeField] Camera PlayerCamera;
+    [SerializeField] CharacterController CharacterController;
+    [Header]
     [SerializeField] float MouseSensitivity = 100f;
     [SerializeField] float MoveSpeed = 5f;
+
     float xRotation = 0f;
     
 
@@ -13,4 +17,19 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
+
+    void Update()
+    {
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
 }
