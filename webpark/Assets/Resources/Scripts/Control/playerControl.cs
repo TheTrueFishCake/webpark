ccb316cc80b9f98f564566cc214a6f3e4bc28a1e
saudecigaming <saudecigaming@gmail.com>
2025-11-06 17:28:08 -0500

    added clunky character controller (camera, movement, jumping)

---
+++ webpark/Assets/Resources/Scripts/Control/playerControl.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    
+    Rigidbody rb;
+
+    SphereCollider floorDetectorCol;
+
+    Transform cam;
+
+    [Range(0f, 10.0f)]
+    [SerializeField] float moveSpeed = 7f;
+    [Range(0f, 15.0f)]
+    [SerializeField] float jumpHeight = 10f;
+
+    [Space]
+
+    [Range(0f,10f)]
+    [SerializeField] float cameraSensitivityX = 5f;
+    [Range(0f, 10f)]
+    [SerializeField] float cameraSensitivityY = 5f; //didn't know how to make these without giving both floats spaces so I made them seperate ;*(
+
+    bool touchingGround = true;
+    float xCamRot,Z;
+    Vector3 movementInp;
+    Vector3 cameraInp;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = FindFirstObjectByType<Camera>().GetComponent<Transform>(); //please dont make fun of me for this
+        floorDetectorCol = GetComponent<SphereCollider>();
+    }
+
+    void Update()
+    {
+
+        // MOVEMENT //
+
+        movementInp = new Vector3 (Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        Vector3 MoveVector = transform.TransformDirection(movementInp) * moveSpeed;  // allowing direction to change based on rotation
+        rb.linearVelocity = new Vector3(MoveVector.x, rb.linearVelocity.y, MoveVector.z); //movement
+
+
+        // JUMPING //
+
+
+        if (Input.GetButtonDown("Jump") && touchingGround)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpHeight, rb.linearVelocity.z); //adds jump velocity
+            touchingGround = false; //disallow jumping
+        }
+
+        // CAMERA //
+
+        cameraInp = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        xCamRot -= cameraInp.y * cameraSensitivityY;
+
+        transform.Rotate(0f,cameraInp.x * cameraSensitivityX, 0f);
+        cam.localRotation = Quaternion.Euler(xCamRot,0f,0f);
+
+        //camera rotation currently needs to be clamped on Y
+    }
+
+    private void OnCollisionEnter(Collision collision) //onCollisionEnter so that it will only detect when falling onto ground and not when in the middle of jumping up
+    {
+        touchingGround = true; //if sphere collider is touching anything, set touchingGround to true to allow jumping
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        touchingGround = false; //if off ground, disallow jumping
+    }
+}
