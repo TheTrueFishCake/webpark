52ff516a814355297c3767e91ed17088e91678d6
TheTrueFishCake <fishcake2000yt@gmail.com>
2025-11-07 17:37:18 -0500

    revised things

--- webpark/Assets/Scripts/PlayerController.cs
+++ webpark/Assets/Scripts/PlayerController.cs
@@ -2,34 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("References")]]
-    [SerializeField] Camera PlayerCamera;
-    [SerializeField] CharacterController CharacterController;
-    [Header]
-    [SerializeField] float MouseSensitivity = 100f;
-    [SerializeField] float MoveSpeed = 5f;
+    [Header("References")]
+    [SerializeField] CharacterController characterController;
+    [SerializeField] Camera playerCamera;
+    [Header("Movement Settings")]
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpHeight = 2f;
 
-    float xRotation = 0f;
-    
-
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    void Update()
-    {
-    }
-
-    void HandleMouseLook()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        PlayerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        transform.Rotate(Vector3.up * mouseX);
-    }
 
 }
+
---
+++ webpark/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    SphereCollider floorDetectorCol;
+
+    Transform cam;
+
+    [Range(0f, 10.0f)]
+    [SerializeField] float moveSpeed = 7f;
+    [Range(0f, 15.0f)]
+    [SerializeField] float jumpHeight = 10f;
+
+    [Space]
+
+    [Range(0f, 10f)]
+    [SerializeField] float cameraSensitivityX = 5f;
+    [Range(0f, 10f)]
+    [SerializeField] float cameraSensitivityY = 5f; //didn't know how to make these without giving both floats spaces so I made them seperate ;*(
+
+    bool touchingGround, canJump;
+    float xCamRot, extraJumpTime;
+    Vector3 movementInp;
+    Vector3 cameraInp;
+
+    void Start()
+    {
+        Cursor.visible = false;
+
+        rb = GetComponent<Rigidbody>();
+        cam = FindFirstObjectByType<Camera>().GetComponent<Transform>(); //please dont make fun of me for this
+        floorDetectorCol = GetComponent<SphereCollider>();
+    }
+
+    void Update()
+    {
+
+        // MOVEMENT //
+
+        movementInp = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        Vector3 MoveVector = transform.TransformDirection(movementInp) * moveSpeed;  // allowing direction to change based on rotation
+        rb.linearVelocity = new Vector3(MoveVector.x, rb.linearVelocity.y, MoveVector.z); //movement
+
+
+        // JUMPING //
+
+        if (extraJumpTime >= 0 && !touchingGround)
+            extraJumpTime -= Time.deltaTime;
+        else if (extraJumpTime <= 0 && !touchingGround)
+            canJump = false;
+
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpHeight, rb.linearVelocity.z); //adds jump velocity
+            touchingGround = false; //disallow jumping
+        }
+
+        // CAMERA //
+
+        cameraInp = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        xCamRot -= cameraInp.y * cameraSensitivityY;
+
+        transform.Rotate(0f, cameraInp.x * cameraSensitivityX, 0f);
+        cam.localRotation = Quaternion.Euler(xCamRot, 0f, 0f);
+
+        //camera rotation currently needs to be clamped on Y
+    }
+
+    private void OnTriggerEnter() //onCollisionEnter so that it will only detect when falling onto ground and not when in the middle of jumping up
+    {
+        Debug.Log("can jump");
+        touchingGround = true; //if sphere collider is touching anything, set touchingGround to true and allow jumping
+        canJump = true;
+    }
+
+    private void OnTriggerExit()
+    {
+        extraJumpTime = 0.25f;
+        touchingGround = false; //if off ground, disallow jumping after a 0.25s timer
+    }
+}
